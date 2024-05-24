9717f84943d333a2235476012cebc987e8e9bedc
tyemilen <ax4659857@gmail.com>
2024-05-25 01:40:54 +0300

    fix: remove useless code/comments

--- src/kernel.c
+++ src/kernel.c
@@ -16,16 +16,5 @@ void kernel_main(void) {
 	display_write_string("+ Done!\n", 10);
 	display_enable_cursor();
 
-	// char c = 0;
-
 	shell_loop();
-	while (1) {
-		// c = keyboard_get_key();
-
-		// if (c > 0) {
-		// 	putch(c);
-		// 	c = 0;
-		// 	continue;
-		// }
-	}
 }
